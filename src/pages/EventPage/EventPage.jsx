00c1d3d1ea1f17e5c60fa4174ee6a0cb60746f6e
Gurpreet Brar <guribrar200@gmail.com>
2025-02-20 01:48:27 -0500

    add event page and created function to get event details

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -1,6 +1,27 @@
 import "./EventPage.scss";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function EventPage() {
+  const { id } = useParams();
+  const [event, setEvent] = useState(null);
+  const base_url = import.meta.env.VITE_API_URL;
+
+  const getEvent = async () => {
+    try {
+      const response = await axios.get(`${base_url}/events/${id}`);
+      console.log(response.data);
+      setEvent(response.data);
+    } catch (error) {
+      console.log("Error getting event details", error);
+    }
+  };
+
+  useEffect(() => {
+    getEvent();
+  }, [id]);
+
   return <main className="event__main"></main>;
 }
 
