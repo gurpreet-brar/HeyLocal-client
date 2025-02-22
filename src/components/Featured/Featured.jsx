d6a6f11c78aae1f4980349a5056f8cafbe820d4b
Gurpreet Brar <guribrar200@gmail.com>
2025-02-21 23:12:46 -0500

    add styling to top featured events on home page

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,7 +1,9 @@
 import "./Featured.scss";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 
 function Featured() {
@@ -29,6 +31,31 @@ function Featured() {
   useEffect(() => {
     getFeaturedEvents();
   }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
     <section className="featured">
       <h3 className="featured__title">Top events</h3>
@@ -54,16 +81,25 @@ function Featured() {
                 View Event
               </a>
             </div>
-            <div className="featured__carousel">
-              {featuredEvents.map((event) => (
+            <Slider {...settings} className="featured__carousel">
+              {featuredEvents.slice(1).map((event) => (
                 <div key={event.event_id} className="featured__event-card">
-                  <img src={event.image_url} alt={event.title} />
-                  <h3>{event.title}</h3>
-                  <p>{getDate(event.date)}</p>
-                  <Link to={`/event/${event.event_id}`}>View Details</Link>
+                  <img
+                    className="event-card__image"
+                    src={event.image_url}
+                    alt={event.title}
+                  />
+                  <h3 className="event-card__title">{event.title}</h3>
+                  <p className="event-card__info">{getDate(event.date)}</p>
+                  <Link
+                    className="event-card__link"
+                    to={`/event/${event.event_id}`}
+                  >
+                    View Details
+                  </Link>
                 </div>
               ))}
-            </div>
+            </Slider>
           </div>
         )}
       </div>
